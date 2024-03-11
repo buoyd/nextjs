cd735e0fa2802ab676030fbf7f877ffa7f73b687
thanh-phongn <thanh-phongn@system-exe.com.vn>
2024-03-11 18:04:37 +0700

    finish chaper 9

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,8 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+
+          <Link href="/ui/dashboard">Dashboard</Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
